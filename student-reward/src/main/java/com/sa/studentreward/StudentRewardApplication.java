bd93e8ca8b9db49a69ec581bf83fe69de0b7efaf
SrijanaLama2 <srijana@Srijanas-MacBook-Pro.local>
2023-02-07 10:26:38 -0600

    student-reward

---
+++ student-reward/src/main/java/com/sa/studentreward/StudentRewardApplication.java
@@ -0,0 +1,13 @@
+package com.sa.studentreward;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentRewardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentRewardApplication.class, args);
+	}
+
+}
