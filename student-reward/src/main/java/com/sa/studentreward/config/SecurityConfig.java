88dfa541964acbf47c01a43a191a696d8d6b17b6
Bipul Ranjitkar <branjitkar@users.noreply.github.com>
2023-02-08 00:09:55 -0600

    fixed StudentRewardApplication missing dependency

--- student-reward/src/main/java/com/sa/studentreward/config/SecurityConfig.java
+++ student-reward/src/main/java/com/sa/studentreward/config/SecurityConfig.java
@@ -1,7 +1,11 @@
 package com.sa.studentreward.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
@@ -14,6 +18,11 @@ public class SecurityConfig extends ResourceServerConfigurerAdapter {
                 .authorizeRequests()
                 .anyRequest()
                 .authenticated();
+    }
+
+    @Bean
+    public OAuth2RestTemplate oauth2RestTemplate(OAuth2ClientContext oauth2ClientContext, OAuth2ProtectedResourceDetails details) {
+        return new OAuth2RestTemplate(details, oauth2ClientContext);
     }
 
 }
